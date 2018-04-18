42d830ac11a3abe3c61f617e7a5101b268fbdf08
ImadLakehal <36035787+ImadLakehal@users.noreply.github.com>
2018-04-18 10:16:22 +0200

    Update RObserver.js

--- RObserver.js
+++ RObserver.js
@@ -1,33 +1,17 @@
 // Only run if ResizeObserver is supported.
     if ('ResizeObserver' in self) {
-        alert ("I m a Super Chrome Browser");
+        //alert ("I m a Super Chrome Browser");
         // Create a single ResizeObserver instance to handle all
         // container elements. The instance is created with a callback,
         // which is invoked as soon as an element is observed as well
         // as any time that element's size changes.
         var ro = new ResizeObserver(function(entries) {
-            // Default breakpoints that should apply to all observed
-            // elements that don't define their own custom breakpoints.  ***  OPTIONAL   ***
-            var defaultBreakpoints = {SM: 384, MD: 576, LG: 768, XL: 960};
-
-            alert ("I ve at least picked a targeted element");
-
+            //alert ("I ve at least picked a targeted element");
             entries.forEach(function(entry) {
                 // If breakpoints are defined on the observed element,
                 // use them. Otherwise use the defaults.   ***  OPTIONAL   ***
                document.querySelector('body').style.paddingTop = (document.querySelector('ggm-header > header').clientHeight + 10) + 'px';
                var tmp = document.querySelector('body').style.paddingTop;
-               alert("tester : "+ tmp);
-
-                // Update the matching breakpoints on the observed element. ***  OPTIONAL   ***
-                // Object.keys(breakpoints).forEach(function(breakpoint) {
-                //     var minHeight = breakpoints[breakpoint];
-                //     if (entry.contentRect.height >= minHeight) {
-                //         entry.target.classList.add(breakpoint);
-                //     } else {
-                //         entry.target.classList.remove(breakpoint);
-                //     }
-                // });
             });
         });
 
